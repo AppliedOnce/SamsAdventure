60ff9d47fadb3437d657eebf3ae7485e9f91e50f
Mathias Stabell <mattistabell@gmail.com>
2021-03-12 18:43:58 +0100

    Edited the player movement
    Now the player can move and jump as well as it was a camera that follows.

--- Source/SamsAdventure/MainCharacter.h
+++ Source/SamsAdventure/MainCharacter.h
@@ -10,20 +10,28 @@ UCLASS()
 class SAMSADVENTURE_API AMainCharacter : public ACharacter
 {
 	GENERATED_BODY()
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera");
+	class USpringArmComponent* CameraArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera");
+	class UCameraComponent* PlayerCamera;
 
 public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
 };
