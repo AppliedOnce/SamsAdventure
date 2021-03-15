bd8e75db75ed6618dc9ba45e40cab47e5c1956f4
Mathias Stabell <mattistabell@gmail.com>
2021-03-15 17:58:46 +0100

    Added a Health Component and changed running.
    Added a health component to the player that controls when the player takes damage as well as giving the player invulnerability.
    Changed running to utilize the built in character movement component.

---
+++ Source/SamsAdventure/PlayerHealth.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerHealth.h"
+
+// Sets default values for this component's properties
+UPlayerHealth::UPlayerHealth()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerHealth::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlayerHealth::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (bIsInvulnerable)
+	{
+		Timer += DeltaTime;
+
+		if (Timer >= InvulnerableSeconds)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("No longer Invulnerable!"));
+			bIsInvulnerable = false;
+			Timer = 0.f;
+		}
+	}
+}
+
+void UPlayerHealth::LoseHp(int ammount)
+{
+	if (!bIsInvulnerable)
+	{
+		if (CurrentHealth <= 0)
+			CurrentHealth = 0;
+		else
+			CurrentHealth -= ammount;
+
+		bIsInvulnerable = true;
+	}
+}
+
+void UPlayerHealth::IncreaseHp(int ammount)
+{
+	if (CurrentHealth >= MaxHealth)
+		CurrentHealth = MaxHealth;
+	else
+		CurrentHealth += ammount;
+}
+
+int UPlayerHealth::GetCurrentHp()
+{
+	return CurrentHealth;
+}
