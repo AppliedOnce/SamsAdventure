5596dfc881a6790bb6086f6d9c6e697cf1484736
Mathias Stabell <mattistabell@gmail.com>
2021-04-15 21:17:07 +0200

    First version of the level

--- Source/SamsAdventure/DefenseNut.cpp
+++ Source/SamsAdventure/DefenseNut.cpp
@@ -9,6 +9,8 @@ ADefenseNut::ADefenseNut()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Collider = CreateDefaultSubobject<USphereCompoonent>(TEXT("Collider"));
+	RootComponent = Collider;
 }
 
 // Called when the game starts or when spawned
