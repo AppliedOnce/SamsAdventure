e00a03ef6eaa60df56cd0436aa10884f535c3f51
OleSkalstad <ole.skalstad@icloud.com>
2021-05-19 19:00:51 +0200

    animasjon update

--- Source/SamsAdventure/MainCharacter.cpp
+++ Source/SamsAdventure/MainCharacter.cpp
@@ -60,8 +60,8 @@ void AMainCharacter::BeginPlay()
 void AMainCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 	TimeSinceLastShot += DeltaTime;
+	JumpAnim = MovementComp->IsFalling();
 	if (!MovementComp->IsFalling() && Paraglider->IsVisible())
 	{
 		Paraglider->SetVisibility(false, true);
@@ -76,7 +76,6 @@ void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	PlayerInputComponent->BindAxis("Forward", this, &AMainCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("Right", this, &AMainCharacter::MoveRight);
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMainCharacter::AnimateJump);
 	PlayerInputComponent->BindAction("Glide", IE_Pressed, this, &AMainCharacter::Glide);
 	PlayerInputComponent->BindAction("Glide", IE_Released, this, &AMainCharacter::StopGliding);
 	PlayerInputComponent->BindAction("Shoot", IE_Pressed, this, &AMainCharacter::Shoot);
@@ -84,10 +83,7 @@ void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	PlayerInputComponent->BindAction("Run", IE_Pressed, this, &AMainCharacter::Run);
 	PlayerInputComponent->BindAction("Run", IE_Released, this, &AMainCharacter::StopRunning);
 }
-void AMainCharacter::AnimateJump()
-{
-	JumpAnim = true;
-}
+
 
 
 void AMainCharacter::MoveForward(float Value)
